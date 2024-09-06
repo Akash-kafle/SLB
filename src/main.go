eebd5beb93f73bda797d2fc1946996c8cb039b6e
Akash kafle <aakashkafle47@gmail.com>
2024-09-06 21:30:16 +0545

    Initial touch

--- src/main.go
+++ src/main.go
@@ -2,70 +2,80 @@ package main
 
 import (
 	"fmt"
-	"net/url"
-
-	"github.com/eiannone/keyboard"
+	"io"
+	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 const UNIVERSAL_PROXY = "localhost:5000"
 
 // main file for load balancer
-
 func main() {
-	proxyurl, err := GetProxy()
-	for err != nil {
-		proxyurl, err = GetProxy()
+	server_for_lb := []Server{ // Todo: this is to be replaced by GetServer and we take input from the user
+		*SimpleServer("https://www.google.com"),
+		*SimpleServer("https://www.bing.com"),
+		*SimpleServer("https://www.youtube.com"),
+		*SimpleServer("https://duckduckgo.com"),
 	}
-	if proxyurl == nil {
-		for proxyurl == nil || err != nil {
-			proxyurl, err = url.Parse(UNIVERSAL_PROXY)
-		}
+
+	if len(server_for_lb) <= 1 {
+		fmt.Printf("No server working")
+		return
+	}
+	lb := LoadBalancernew("8000", server_for_lb)
+	handleredirect := func(w http.ResponseWriter, r *http.Request) {
+		lb.ServeProxy(w, r)
+	}
+	http.HandleFunc("/", handleredirect)
+	http.ListenAndServe(":"+lb.port, nil)
+}
+
+func getServer(w io.Writer) (*string, error) {
+	input := ""
+
+	fmt.Printf("Please paste or type your URL (press 'Esc' to exit):")
+	fmt.Scan(input)
+	return &input, nil
+}
+
+func clearTerminal(writer io.Writer) {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+	// Run the command and capture its output
+	cmd.Stdout = writer
+	cmd.Stderr = writer
+	err := cmd.Run()
+	if err != nil {
+		writer.Write([]byte("Failed to clear terminal: " + err.Error()))
 	}
 }
 
-func GetProxy() (*url.URL, error) {
-	var input string
-
-	// Initialize the keyboard listener
-	if err := keyboard.Open(); err != nil {
-		fmt.Println("Failed to open keyboard listener:", err)
-		return nil, err
-	}
-	defer keyboard.Close()
-
-	fmt.Println("Enter URL (Press Esc to cancel):")
-
-	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			fmt.Println("Error reading key:", err)
-			return nil, err
-		}
-
-		if key == keyboard.KeyEsc {
-			fmt.Println("Esc key pressed. Exiting.")
-			return nil, nil
-		}
-
-		if key == keyboard.KeyEnter {
-			break
-		}
-
-		input += string(char)
-		fmt.Print(string(char))
-	}
-
-	parsedURL, err := url.Parse(input)
+func GetServer(writer io.Writer) []Server {
+	var number_of_server int
+	var server_to_return []Server
+	fmt.Printf("Enter the number server to set up:")
+	_, err := fmt.Scan(&number_of_server)
 	if err != nil {
-		fmt.Println("Invalid URL. Please try again.")
-		return GetProxy()
+		var user int
+		fmt.Printf("Error reading inputL %v\n Do you want to retry(1->yes or 0->no): ", err)
+		_, err := fmt.Scan(&user)
+		if err != nil && user != 1 {
+			return []Server{}
+		}
+		getServer(writer)
 	}
-
-	alive := IsBackendAlive(parsedURL)
-	if !alive {
-		fmt.Println("The provided URL's server is down. Please provide a new URL.")
-		return GetProxy()
+	for i := 0; i < number_of_server; i++ {
+		url, err := getServer(writer)
+		if err != nil {
+			fmt.Printf("Error while getting server: %v\n", err)
+		}
+		server_to_return = append(server_to_return, *SimpleServer(*url))
 	}
-
-	return parsedURL, nil
+	return server_to_return
 }
